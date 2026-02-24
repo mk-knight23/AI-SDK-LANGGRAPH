17fd4245f3c5405ee969b276008c4002c2cf77b2
mk.knight23 <mk.knight23@example.com>
2026-02-24 17:56:25 +0530

    chore: update .gitignore to exclude build artifacts and cache files

--- build/server/chunks/1-CEgWv23-.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-7QJAskqO.js')).default;
-const imports = ["_app/immutable/nodes/1.Bu_usHqK.js","_app/immutable/chunks/Cp8qDwIr.js","_app/immutable/chunks/BlsAYwGw.js","_app/immutable/chunks/BYAeq14Y.js","_app/immutable/chunks/CBpHryqH.js","_app/immutable/chunks/tim91OU0.js","_app/immutable/chunks/CUg8qQUu.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-CEgWv23-.js.map
--- build/server/chunks/2-Da9Z6yCv.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-CETIDLNd.js')).default;
-const imports = ["_app/immutable/nodes/2.BMD-f5z0.js","_app/immutable/chunks/Cp8qDwIr.js","_app/immutable/chunks/BlsAYwGw.js","_app/immutable/chunks/BYAeq14Y.js"];
-const stylesheets = ["_app/immutable/assets/2.DCU77cEF.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-Da9Z6yCv.js.map
